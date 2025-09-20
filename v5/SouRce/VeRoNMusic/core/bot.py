4c99c3e3b5ca6a0f2ac6332dfe16d195f604f8ff
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2025-09-20 10:49:55 +0300

    Uploaded Successfully

---
+++ v5/SouRce/VeRoNMusic/core/bot.py
@@ -0,0 +1,98 @@
+import uvloop
+
+uvloop.install()
+
+
+import sys
+
+from pyrogram import Client
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import (
+    BotCommand,
+    BotCommandScopeAllChatAdministrators,
+    BotCommandScopeAllGroupChats,
+    BotCommandScopeAllPrivateChats,
+)
+
+import config
+
+from ..logging import LOGGER
+
+
+class KIMBot(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot")
+        super().__init__(
+            "VeRoNMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.mention = self.me.mention
+
+        try:
+            await self.send_video(
+                chat_id=config.LOG_GROUP_ID,
+                video="https://h.uguu.se/NuRwKkaV.mp4",
+                caption=f"<b>⬢» {self.mention}\n╮⭗ تم تشغيل البـوت :\n╯⦿ علي سورس فيࢪون :\n╭⭗ɴᴀᴍᴇ : {self.name}\n│᚜⦿ᴜꜱᴇʀ ɴᴀᴍᴇ : @{self.username}\n╰⭗ɪᴅ : {self.id}</b>",
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+        if config.SET_CMDS == str(True):
+            try:
+
+                await self.set_bot_commands(
+                    commands=[
+                        BotCommand("start", "sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ"),
+                        BotCommand("help", "ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ"),
+                        BotCommand("KIM", "ᴄᴇᴏ ᴠᴇɢᴀ"),
+                        BotCommand("ping", "ᴄʜᴇᴄᴋ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ"),
+                    ],
+                    scope=BotCommandScopeAllPrivateChats(),
+                )
+                await self.set_bot_commands(
+                    commands=[
+                        BotCommand("play", "sᴛᴀʀᴛ ᴘʟᴀʏɪɴɢ ʀᴇǫᴜᴇᴛᴇᴅ sᴏɴɢ"),
+                    ],
+                    scope=BotCommandScopeAllGroupChats(),
+                )
+                await self.set_bot_commands(
+                    commands=[
+                        BotCommand("play", "sᴛᴀʀᴛ ᴘʟᴀʏɪɴɢ ʀᴇǫᴜᴇᴛᴇᴅ sᴏɴɢ"),
+                        BotCommand("skip", "ᴍᴏᴠᴇ ᴛᴏ ɴᴇxᴛ ᴛʀᴀᴄᴋ ɪɴ ǫᴜᴇᴜᴇ"),
+                        BotCommand("pause", "ᴘʟᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏɪɴɢ sᴏɴɢ"),
+                        BotCommand("resume", "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴘᴀᴜsᴇᴅ sᴏɴɢ"),
+                        BotCommand("end", "ᴄʟᴇᴀʀ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀᴍᴅ ʟᴇᴀᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛ"),
+                        BotCommand("shuffle", "Rᴀɴᴅᴏᴍʟʏ sʜᴜғғʟᴇs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴘʟᴀʏʟɪsᴛ."),
+                        BotCommand("KIM", "ᴄᴇᴏ ᴠᴇɢᴀ"),
+                        BotCommand(
+                            "playmode",
+                            "Aʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴅᴇғᴀᴜʟᴛ ᴘʟᴀʏᴍᴏᴅᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ",
+                        ),
+                        BotCommand(
+                            "settings",
+                            "Oᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴏғ ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.",
+                        ),
+                    ],
+                    scope=BotCommandScopeAllChatAdministrators(),
+                )
+            except:
+                pass
+        else:
+            pass
+   
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"ᴍᴜɪꜱᴄ»ʙᴏᴛ ꜱᴛᴀʀᴛɪɢ🔥 as {self.name}")
